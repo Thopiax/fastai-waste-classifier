e181b13f790ca256f5de21e2fd5844af4086b450
Rafael T. Ballestiero <rafa.ballestiero@gmail.com>
2020-04-19 18:10:51 +0200

    Update Model
    Change the default classes and model file to reflect the model trained on the TrashNet dataset.

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/gbtwqqofilxfj9a/model.pkl?dl=1'
+export_file_name = 'model.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['metal', 'glass', 'plastic', 'paper', 'cardboard', 'trash']
 path = Path(__file__).parent
 
 app = Starlette()
